8585e50806dc62aae652f34485197721ade06f11
bookworm-coding <bookwormkkm@proton.me>
2025-03-28 18:47:02 +0900

    Init

---
+++ lib/calendar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatelessWidget{
+  const Calendar({super.key});
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/nav_bar.dart
@@ -0,0 +1,67 @@
+import 'package:dimiplanner/home.dart';
+import 'package:dimiplanner/calendar.dart';
+import 'package:dimiplanner/account.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Nav extends StatefulWidget {
+  const Nav({super.key});
+
+  @override
+  State<Nav> createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  int currentIndex = 0;
+  final screens = [const Homepage(), const Calendar(), const Account()];
+  bool mark=false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        toolbarHeight: 70,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColorLight,
+        title: SvgPicture.asset(
+          'assets/icons/logo_rectangular.svg',
+          height: 50,
+          fit: BoxFit.contain,
+        ),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(60),
+          topRight: Radius.circular(60),
+        ),
+        child: NavigationBar(
+            backgroundColor: Theme.of(context).primaryColorLight,
+            surfaceTintColor: Theme.of(context).primaryColor,
+            selectedIndex: currentIndex,
+            onDestinationSelected: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            destinations: const <Widget>[
+              NavigationDestination(
+                icon: Icon(Icons.home_rounded),
+                label: '홈',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.calendar_month_rounded),
+                label: '캘린더',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.account_circle_rounded),
+                label: '계정관리',
+              )
+            ]),
+      ),
+      body: screens[currentIndex],
+    );
+  }
+}
