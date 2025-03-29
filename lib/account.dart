259c730123e25bd805d5e6acf6d5acff265ae5ea
bookworm-coding <bookwormkkm@proton.me>
2025-03-29 16:19:26 +0900

    윈도우, 리눅스, 맥 지원 제거 계정 관리(미완) 추가 캘린더 -> 플래너 변경

--- lib/account.dart
+++ lib/account.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
+import 'package:webview_flutter_web/webview_flutter_web.dart';
 
-class Account extends StatelessWidget{
+class Account extends StatefulWidget{
   const Account({super.key});
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
+  State<Account> createState() => _AccountState();
+}
 
-    );
+class _AccountState extends State<Account>{
+  String? sessionId;
+  static const String backend = "dimigo.co.kr:3000";
+
+  void checkLogin() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    sessionId = prefs.getString('session_id');
+    if(sessionId==null){
+      prefs.setString('session_id', login());
+    }
+    else{
+      var api = Uri.https(backend, '/api/user/registered');
+      var response = await http.get(api, headers: {'cookie' : "session.id=${sessionId!}"});
+      if(response.statusCode == 200){
+        return;
+      }
+      else{
+        prefs.setString('session_id', login());
+      }
+    }
+  }
+
+  String login(){
+    // TODO create login
+    return '';
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp();
   }
 }
