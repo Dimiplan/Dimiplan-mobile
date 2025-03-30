a4b92e5019a088680118030a491b49c50b554510
bookworm-coding <bookwormkkm@proton.me>
2025-03-30 09:37:30 +0900

    플래너 추가 (DB unimplemented)

--- lib/internal/model.dart
+++ lib/internal/model.dart
@@ -1,9 +1,9 @@
 class Task {
-  int id;
+  int? id;
   String title;
   DateTime date;
   String priority;
   bool status; // true : processing, false : finished
 
-  Task({required this.id, required this.title, required this.date, required this.priority, required this.status});
+  Task({this.id, required this.title, required this.date, required this.priority, this.status=true});
 }
---
+++ lib/views/add_task.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+import 'package:dimiplanner/internal/database.dart';
+import 'package:dimiplanner/internal/model.dart';
+import 'package:dimiplanner/views/planner.dart';
+import 'package:intl/intl.dart';
+
+class AddTaskScreen extends StatefulWidget {
+  final Function updateTaskList;
+  final Task? task;
+
+  const AddTaskScreen({super.key, required this.updateTaskList, this.task});
+
+  @override
+  State<AddTaskScreen> createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = '';
+  late String _priority;
+  DateTime _date = DateTime.now();
+  final TextEditingController _dateController = TextEditingController();
+  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+
+  final List<String> _priorities = ['Low', 'Medium', 'High'];
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.task != null) {
+      _title = widget.task!.title;
+      _date = widget.task!.date;
+      _priority = widget.task!.priority;
+    }
+
+    _dateController.text = _dateFormatter.format(_date);
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  _handleDatePicker() async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _dateFormatter.format(date);
+    }
+  }
+
+  _delete() {
+    // TODO DB DatabaseHelper.instance.deleteTask(widget.task!.id!);
+    Navigator.pop(context);
+    widget.updateTaskList();
+  }
+
+  _submit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('$_title, $_date, $_priority');
+
+      Task task = Task(title: _title, date: _date, priority: _priority);
+      if (widget.task == null) {
+        // TODO DB DatabaseHelper.instance.insertTask(task);
+      } else {
+        // Update the task
+        task.id = widget.task!.id;
+        task.status = widget.task!.status;
+        // TODO DB DatabaseHelper.instance.updateTask(task);
+      }
+      Navigator.push(context, MaterialPageRoute(builder: (_) => Planner()));
+      widget.updateTaskList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () => Navigator.pop(context)),
+        title: Row(children: [
+          Text(
+            widget.task == null ? 'Add Task' : 'Update Task',
+            style: const TextStyle(
+              color: Colors.redAccent,
+              fontSize: 20.0,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ]),
+        // actions: [
+        //   IconButton(
+        //       icon: Icon(
+        //         Icons.info_outline,
+        //         color: Colors.black,
+        //       ),
+        //       onPressed: () {}),
+        // ],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Title',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => input!.trim().isEmpty
+                              ? 'Please enter a task title'
+                              : null,
+                          onSaved: (input) => _title = input!,
+                          initialValue: _title,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          controller: _dateController,
+                          style: TextStyle(fontSize: 18.0),
+                          onTap: _handleDatePicker,
+                          decoration: InputDecoration(
+                            labelText: 'Date',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: DropdownButtonFormField(
+                          isDense: true,
+                          icon: Icon(Icons.arrow_drop_down_circle),
+                          iconSize: 22.0,
+                          iconEnabledColor: Theme.of(context).primaryColor,
+                          items: _priorities.map((String priority) {
+                            return DropdownMenuItem(
+                              value: priority,
+                              child: Text(
+                                priority,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                          style: TextStyle(fontSize: 18.0),
+                          decoration: InputDecoration(
+                            labelText: 'Priority',
+                            labelStyle: TextStyle(fontSize: 18.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          validator: (input) => _priority == null
+                              ? 'Please select a priority level'
+                              : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _priority = value!;
+                            });
+                          },
+                          value: _priority,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 20.0),
+                        height: 60.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        // ignore: deprecated_member_use
+                        child: TextButton(
+                          onPressed: _submit,
+                          child: Text(
+                            widget.task == null ? 'Add' : 'Update',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      widget.task != null
+                          ? Container(
+                        margin: EdgeInsets.symmetric(vertical: 0.0),
+                        height: 60.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        // ignore: deprecated_member_use
+                        child: TextButton(
+                          onPressed: _delete,
+                          child: Text(
+                            'Delete',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      )
+                          : SizedBox.shrink(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
