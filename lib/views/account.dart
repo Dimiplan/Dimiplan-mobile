bac26f5fa1085e35cba4562180dcd9adf6aa68b6
bookworm-coding <bookwormkkm@proton.me>
2025-04-06 14:58:15 +0900

    로그인 해결

--- lib/views/account.dart
+++ lib/views/account.dart
@@ -1,44 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fetch_client/fetch_client.dart';
-import 'package:webview_flutter_web/webview_flutter_web.dart';
+import 'package:http/http.dart' as http;
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:convert';
 
-class Account extends StatefulWidget{
+class Account extends StatefulWidget {
   const Account({super.key});
   @override
   State<Account> createState() => _AccountState();
 }
 
-class _AccountState extends State<Account>{
+class _AccountState extends State<Account> {
   String? session;
   static const String backend = "dimigo.co.kr:3000";
-  var fetcher = FetchClient();
 
-  void checkLogin() async{
+  void checkLogin() async {
     var prefs = await SharedPreferences.getInstance();
     session = prefs.getString('session');
-    if(session==null || session == ''){
-      prefs.setString('session', login());
-    }
-    else{
+    if (session == null || session == '') {
+      prefs.setString('session', await login() ?? '');
+    } else {
       var api = Uri.https(backend, '/api/user/registered');
-      var response = await fetcher.get(api, headers: {'cookie' : "connect.sid=${session!}"});
-      if(response.statusCode == 200){
+      var response = await http.get(api, headers: {'cookie': session!});
+      if (response.statusCode == 200) {
         return;
-      }
-      else{
-        prefs.setString('session', login());
+      } else {
+        prefs.setString('session', await login() ?? '');
+        session = prefs.getString('session');
       }
     }
   }
 
-  String login(){
-    // TODO create login
-    return '';
+  Future<String?> login() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    var api = Uri.https(backend, '/auth/login');
+    Map<String, dynamic> body = {'userId': googleUser?.id};
+    String jsonBody = json.encode(body);
+    var response = await http.post(
+      api,
+      body: jsonBody,
+      headers: {'Content-Type': 'application/json'},
+    );
+    return response.headers['set-cookie'];
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     checkLogin();
     return MaterialApp();
   }
