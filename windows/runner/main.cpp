c8ff7eafbaf2708fc0d7baf66f6054fdd155e275
bookworm-coding <bookwormkkm@proton.me>
2025-04-30 09:38:45 +0900

    이름 수정

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"dimiplan", origin, size)) {
+  if (!window.Create(L"디미플랜", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
